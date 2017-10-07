0043cbdf35a7f03483a3357c9eb0437637c6d57f
bill <david@localhost.localdomain>
2017-10-07 00:48:04 -0700

    remove extension

--- lvm.py
+++ lvm.py
@@ -1,42 +1,35 @@
-    #! /usr/bin/python
+#! /usr/bin/python
 
 from optparse import OptionParser
 from subprocess import Popen, call, PIPE
 from shlex import split
-    #to import getstatusoutput from commands lib
 from commands import getstatusoutput
-import os
 from os.path import basename, normpath
-import shlex, subprocess
+import subprocess
 import time
+import os
 
 devnull = open(os.devnull, 'w')
 parser = OptionParser()
-parser.add_option('-d', '--drive', dest='drive', help='this is the name of the drive like sda,hdd')
-parser.add_option('-v', '--vgfolder1', dest='vgfolder', help='this is the volume group name')
-parser.add_option('-l', '--lvfolder', dest='lvfolder', help='this is the logical volume group name')
+parser.add_option('-d', '--drive', dest='drive')
+parser.add_option('-v', '--vgfolder1', dest='vgfolder')
+parser.add_option('-l', '--lvfolder', dest='lvfolder')
 (opts, args) = parser.parse_args()
 
 class addDrive(object):
-
     def __init__(self, drive, vgfolder, lvfolder):
         self.drive = str(drive)
         self.vgfolder = str(vgfolder)
         self.lvfolder = str(lvfolder)
 
     def check_disk(self):
-        flags=False
         cmd = "cat /proc/partitions | awk '{print $4}'"
         ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output= ps.communicate()[0].split(", \n")
+        output = ps.communicate()[0].split(", \n")
         drives = self.drive+"1"
         if drives in str(output):
             print "Disk has been added to lvm, please double check disk partitions table %s" % output
             exit(-1)
-            flags = True
-        else:
-            flags = False
-            print "Starting jobs"
     def fdisk(self):
         before_format_cmd = 'echo -e "n\np\n1\n\n\nt\n8e\\nw\n"'
         before_format_arg = split(before_format_cmd)
@@ -44,7 +37,6 @@ class addDrive(object):
         after_format_arg = split(after_format_cmd)
         p1 = Popen(before_format_arg, stdout=PIPE)
         p2 = call(after_format_arg, stdin=p1.stdout, stderr=devnull)
-        #p2 = call(after_format_arg, stdin=p1.stdout)
         if p2 != 0:
             print 'disk not valid /dev/%s' %self.drive
             exit(-1)
@@ -61,26 +53,21 @@ class addDrive(object):
         vgextend = 'vgextend %s /dev/%s1' % (basename(normpath(self.vgfolder)),self.drive)
         status,output = getstatusoutput(vgextend)
         print "This is for vg"
-        print  status,output
+        print status,output
     def extend_lv(self):
         lvextend = call(["lvextend", "-l", "+100%FREE", "/dev/%s/%s" % (basename(normpath(self.vgfolder)),basename(normpath(self.lvfolder)))])
     def resizefs(self):
-            #rs = 'xfs_growfs /dev/%s/%s' % (basename(normpath(self.vgfolder)),basename(normpath(self.lvfolder)))
         rs = call(["xfs_growfs", "/dev/%s/%s" % (basename(normpath(self.vgfolder)),basename(normpath(self.lvfolder)))])
 def main():
-        x = addDrive(opts.drive, opts.vgfolder, opts.lvfolder)
-        x.check_disk()
-        x.fdisk()
-        x.partprobe()
-        x.pvcreate()
+        x1 = addDrive(opts.drive, opts.vgfolder, opts.lvfolder)
+        x1.check_disk()
+        x1.fdisk()
+        x1.partprobe()
+        x1.pvcreate()
         time.sleep(5)
-        x.extend_vg()
-        x.extend_lv()
-        x.resizefs()
+        x1.extend_vg()
+        x1.extend_lv()
+        x1.resizefs()
 if __name__ == "__main__":
     print "Usage: python lvm.python -d sdb -v cl -l root, which means -d : disk, -v : volume, -l : logical volume"
-
-    if opts.drive == None:
-        #parser.print_help()
-        exit(-1)
-main()
+    main()
